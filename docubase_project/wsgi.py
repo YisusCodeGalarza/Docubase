547faaa6427d570ecfd25bbe8d5624871a4c66cd
Ace Burner <yisuscode@YisusCode.YisusCode>
2025-10-08 17:25:01 -0700

    Ejecutar migraciones desde wsgi.py para Render

--- docubase_project/wsgi.py
+++ docubase_project/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for docubase_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docubase_project.settings')
+
+django.setup()
+call_command('migrate')
 
 application = get_wsgi_application()
